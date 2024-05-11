5646ff8cafe24fe3292090deee774ea18377b5e9
FatemehAria <ftemewh@gmail.com>
2024-05-11 15:30:48 +0330

    Hero component initialized

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <main>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda, omnis eveniet nihil perspiciatis perferendis ut neque. Mollitia nostrum quae magnam? Facilis excepturi, tempora consequatur pariatur odio vero quam accusamus ad id. Aut, commodi possimus praesentium beatae voluptatum modi asperiores veniam velit recusandae qui alias explicabo magni error similique. Accusamus, culpa.</p>
+      <Hero />
     </main>
   );
 }
