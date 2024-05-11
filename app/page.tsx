cfba9a7bbb4f357b31abec48638e3523fd84659c
FatemehAria <ftemewh@gmail.com>
2024-05-11 15:27:08 +0330

    navbar's initial design done

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda, omnis eveniet nihil perspiciatis perferendis ut neque. Mollitia nostrum quae magnam? Facilis excepturi, tempora consequatur pariatur odio vero quam accusamus ad id. Aut, commodi possimus praesentium beatae voluptatum modi asperiores veniam velit recusandae qui alias explicabo magni error similique. Accusamus, culpa.</p>
     </main>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,20 +9,49 @@ import NavbarListSmall from "./NavbarListSmall";
 
 function Navbar() {
   return (
-    <nav className="flex flex-row justify-between lg:justify-around items-center py-8 text-base">
-      <Image src={Logo} alt="logo" className="w-[100px] lg:w-[189px]"/>
+    <nav className="flex flex-row justify-between lg:justify-around items-center py-8 text-base whitespace-nowrap">
+      <Image src={Logo} alt="logo" className="w-[100px] lg:w-[189px]" />
       <NavbarListSmall />
       <div className="hidden lg:flex flex-row w-[30%] items-center justify-between text-[#a7bb80]">
-        <Link href="" className="hover:border-b hover:border-white hover:text-white">Home</Link>
-        <Link href="" className="hover:border-b hover:border-white hover:text-white">Pages</Link>
-        <Link href="" className="hover:border-b hover:border-white hover:text-white">Contact Us</Link>
-        <Link href="" className="hover:border-b hover:border-white hover:text-white">Blog</Link>
-        <Link href="" className="hover:border-b hover:border-white hover:text-white">Landing</Link>
+        <Link
+          href=""
+          className="hover:border-b hover:border-white hover:text-white"
+        >
+          Home
+        </Link>
+        <Link
+          href=""
+          className="hover:border-b hover:border-white hover:text-white"
+        >
+          Pages
+        </Link>
+        <Link
+          href=""
+          className="hover:border-b hover:border-white hover:text-white"
+        >
+          Contact Us
+        </Link>
+        <Link
+          href=""
+          className="hover:border-b hover:border-white hover:text-white"
+        >
+          Blog
+        </Link>
+        <Link
+          href=""
+          className="hover:border-b hover:border-white hover:text-white"
+        >
+          Landing
+        </Link>
       </div>
       <div className="hidden lg:flex flex-row w-[20%] justify-between items-center relative">
-        <Link href="" className="hover:border-b hover:border-[#a7bb80]">Login / Registration</Link>
+        <Link href="" className="hover:border-b hover:border-[#a7bb80]">
+          Login / Registration
+        </Link>
         <NavbarSeperator />
-        <Link href="" className="hover:border-b hover:border-[#a7bb80]">Book Table</Link>
+        <Link href="" className="hover:border-b hover:border-[#a7bb80]">
+          Book Table
+        </Link>
       </div>
     </nav>
   );
